46da312070d68549678bc3683fa46665c6a6164f
MAbdurahman <mdbdrrhm@gmail.com>
2021-01-22 15:11:55 -0600

    End of chapter 3.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 import Products from './components/Products';
 
@@ -12,11 +13,14 @@ export default class App extends Component {
       firstName: 'Mahdi',
       lastName: 'Abdurrahman'
     }
+    const isValid = false;
+
     return (
       <div className='App'>
         <h1>react-products-app</h1>
         <Products />
         <h2>Hello, {this.formatName(user)}</h2>
+        <Button variant='primary' disabled={!isValid} >Default</Button>
       </div>
     )
   }
