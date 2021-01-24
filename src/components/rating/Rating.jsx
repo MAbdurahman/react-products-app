a9f012292f0c92838e00e80c007e1261a20bcada
MAbdurahman <mdbdrrhm@gmail.com>
2021-01-24 08:01:38 -0600

    Modify color of react-icon star

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -1,38 +1,43 @@
 import React, { Component } from 'react';
+import { IconContext } from "react-icons";
 import { IoIosStar, IoIosStarOutline } from 'react-icons/io';
 import './Rating.css';
 
 export default class Rating extends Component {
+
    render() {
       return (
-         <div>
-            <h2>Rating: {this.props.rating}</h2>
-            {this.props.rating >= 1 ? (
-               <IoIosStar />
-            ) : (
-               <IoIosStarOutline />
-            )}
-            {this.props.rating >= 2 ? (
-               <IoIosStar />
-            ) : (
-                  <IoIosStarOutline />
-               )}
-            {this.props.rating >= 3 ? (
-               <IoIosStar />
-            ) : (
-                  <IoIosStarOutline />
-               )}
-            {this.props.rating >= 4 ? (
-               <IoIosStar />
-            ) : (
-                  <IoIosStarOutline />
-               )}
-            {this.props.rating >= 5 ? (
-               <IoIosStar />
-            ) : (
-                  <IoIosStarOutline />
-               )}
-         </div>
+         <IconContext.Provider value={{ className: 'react-icons-star' }}>
+            <div>
+               <h2>Rating: {this.props.rating}</h2>
+               {this.props.rating >= 1 ? (
+                  <IoIosStar />
+               ) : (
+                     <IoIosStarOutline />
+                  )}
+               {this.props.rating >= 2 ? (
+                  <IoIosStar />
+               ) : (
+                     <IoIosStarOutline />
+                  )}
+               {this.props.rating >= 3 ? (
+                  <IoIosStar />
+               ) : (
+                     <IoIosStarOutline />
+                  )}
+               {this.props.rating >= 4 ? (
+                  <IoIosStar />
+               ) : (
+                     <IoIosStarOutline />
+                  )}
+               {this.props.rating >= 5 ? (
+                  <IoIosStar />
+               ) : (
+                     <IoIosStarOutline />
+                  )}
+            </div>
+         </IconContext.Provider>
+
       )
    };
 };
