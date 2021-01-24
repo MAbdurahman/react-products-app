272410062c33c4fcb86b9e870865975ad2f76109
MAbdurahman <mdbdrrhm@gmail.com>
2021-01-24 07:23:04 -0600

    Add react-icons stars; end of Chapter 3.3

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { IoIosStar, IoIosStarOutline } from 'react-icons/io';
 import './Rating.css';
 
 export default class Rating extends Component {
@@ -6,6 +7,31 @@ export default class Rating extends Component {
       return (
          <div>
             <h2>Rating: {this.props.rating}</h2>
+            {this.props.rating >= 1 ? (
+               <IoIosStar />
+            ) : (
+               <IoIosStarOutline />
+            )}
+            {this.props.rating >= 2 ? (
+               <IoIosStar />
+            ) : (
+                  <IoIosStarOutline />
+               )}
+            {this.props.rating >= 3 ? (
+               <IoIosStar />
+            ) : (
+                  <IoIosStarOutline />
+               )}
+            {this.props.rating >= 4 ? (
+               <IoIosStar />
+            ) : (
+                  <IoIosStarOutline />
+               )}
+            {this.props.rating >= 5 ? (
+               <IoIosStar />
+            ) : (
+                  <IoIosStarOutline />
+               )}
          </div>
       )
    };
