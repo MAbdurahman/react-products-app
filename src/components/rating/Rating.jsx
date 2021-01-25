2a4c747b4a56cef9196188553b1d262c45bbe827
MAbdurahman <mdbdrrhm@gmail.com>
2021-01-25 02:49:17 -0600

    End of chapter 3.5

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -9,35 +9,39 @@ export default class Rating extends Component {
       this.state = {rating: this.props.rating};
    }
 
+   handleClick(ratingValue) {
+      this.setState({rating: ratingValue});
+   }
+
    render() {
       return (
          <IconContext.Provider value={{ className: 'react-icons-star' }}>
             <div>
                <h2>Rating: {this.state.rating}</h2>
                {this.state.rating >= 1 ? (
-                  <IoIosStar />
+                  <IoIosStar onClick={this.handleClick.bind(this, 1)} />
                ) : (
-                     <IoIosStarOutline />
+                     <IoIosStarOutline onClick={this.handleClick.bind(this, 1)} />
                   )}
                {this.state.rating >= 2 ? (
-                  <IoIosStar />
+                  <IoIosStar onClick={this.handleClick.bind(this, 2)} />
                ) : (
-                     <IoIosStarOutline />
+                     <IoIosStarOutline onClick={this.handleClick.bind(this, 2)} />
                   )}
                {this.state.rating >= 3 ? (
-                  <IoIosStar />
+                  <IoIosStar onClick={this.handleClick.bind(this, 3)} />
                ) : (
-                     <IoIosStarOutline />
+                     <IoIosStarOutline onClick={this.handleClick.bind(this, 3)} />
                   )}
                {this.state.rating >= 4 ? (
-                  <IoIosStar />
+                  <IoIosStar onClick={this.handleClick.bind(this, 4)} />
                ) : (
-                     <IoIosStarOutline />
+                     <IoIosStarOutline onClick={this.handleClick.bind(this, 4)} />
                   )}
                {this.state.rating >= 5 ? (
-                  <IoIosStar />
+                  <IoIosStar onClick={this.handleClick.bind(this, 5)} />
                ) : (
-                     <IoIosStarOutline />
+                     <IoIosStarOutline onClick={this.handleClick.bind(this, 5)} />
                   )}
             </div>
          </IconContext.Provider>
