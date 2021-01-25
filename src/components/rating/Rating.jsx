fab9e1e69146eda60859cc48a789fc21e2578ac2
MAbdurahman <mdbdrrhm@gmail.com>
2021-01-25 02:37:31 -0600

    End of chapter 3.4

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -4,33 +4,37 @@ import { IoIosStar, IoIosStarOutline } from 'react-icons/io';
 import './Rating.css';
 
 export default class Rating extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {rating: this.props.rating};
+   }
 
    render() {
       return (
          <IconContext.Provider value={{ className: 'react-icons-star' }}>
             <div>
-               <h2>Rating: {this.props.rating}</h2>
-               {this.props.rating >= 1 ? (
+               <h2>Rating: {this.state.rating}</h2>
+               {this.state.rating >= 1 ? (
                   <IoIosStar />
                ) : (
                      <IoIosStarOutline />
                   )}
-               {this.props.rating >= 2 ? (
+               {this.state.rating >= 2 ? (
                   <IoIosStar />
                ) : (
                      <IoIosStarOutline />
                   )}
-               {this.props.rating >= 3 ? (
+               {this.state.rating >= 3 ? (
                   <IoIosStar />
                ) : (
                      <IoIosStarOutline />
                   )}
-               {this.props.rating >= 4 ? (
+               {this.state.rating >= 4 ? (
                   <IoIosStar />
                ) : (
                      <IoIosStarOutline />
                   )}
-               {this.props.rating >= 5 ? (
+               {this.state.rating >= 5 ? (
                   <IoIosStar />
                ) : (
                      <IoIosStarOutline />
